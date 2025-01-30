45d2e63ad17a60da1e1fb0aba188d5cd2b45a584
KiwiGuy1 <smallninja.jay@gmail.com>
2025-01-30 15:46:02 -0600

    Add project files.

---
+++ SportsPro/Controllers/TechnicianController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SportsPro.Models;
+
+namespace SportsPro.Controllers
+{
+    public class TechnicianController : Controller
+    {
+        private readonly SportsProContext _context;
+
+        public TechnicianController(SportsProContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Technicians
+        public async Task<IActionResult> List()
+        {
+              return _context.Technicians != null ? 
+                          View(await _context.Technicians.ToListAsync()) :
+                          Problem("Entity set 'SportsProContext.Technicians'  is null.");
+        }
+
+        // GET: Technicians/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Technicians == null)
+            {
+                return NotFound();
+            }
+
+            var technician = await _context.Technicians
+                .FirstOrDefaultAsync(m => m.TechnicianID == id);
+            if (technician == null)
+            {
+                return NotFound();
+            }
+
+            return View(technician);
+        }
+
+        // GET: Technicians/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Technicians/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TechnicianID,Name,Email,Phone")] Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(technician);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(List));
+            }
+            return View(technician);
+        }
+
+        // GET: Technicians/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Technicians == null)
+            {
+                return NotFound();
+            }
+
+            var technician = await _context.Technicians.FindAsync(id);
+            if (technician == null)
+            {
+                return NotFound();
+            }
+            return View(technician);
+        }
+
+        // POST: Technicians/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TechnicianID,Name,Email,Phone")] Technician technician)
+        {
+            if (id != technician.TechnicianID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(technician);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TechnicianExists(technician.TechnicianID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(List));
+            }
+            return View(technician);
+        }
+
+        // GET: Technicians/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Technicians == null)
+            {
+                return NotFound();
+            }
+
+            var technician = await _context.Technicians
+                .FirstOrDefaultAsync(m => m.TechnicianID == id);
+            if (technician == null)
+            {
+                return NotFound();
+            }
+
+            return View(technician);
+        }
+
+        // POST: Technicians/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Technicians == null)
+            {
+                return Problem("Entity set 'SportsProContext.Technicians'  is null.");
+            }
+            var technician = await _context.Technicians.FindAsync(id);
+            if (technician != null)
+            {
+                _context.Technicians.Remove(technician);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+
+        private bool TechnicianExists(int id)
+        {
+          return (_context.Technicians?.Any(e => e.TechnicianID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ SportsPro/Models/Technician.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace SportsPro.Models
+{
+    public class Technician
+    {
+        public int TechnicianID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Phone(ErrorMessage = "Invalid Phone Number")]
+        [StringLength(12, MinimumLength = 12, ErrorMessage = "Phone number must be 12 characters long (including dashes)")]
+
+        public string Phone { get; set; }
+
+    }
+}
