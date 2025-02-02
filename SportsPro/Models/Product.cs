d880181edc7b9e8f70e1c35e68feb3aa65fff80d
KiwiGuy1 <smallninja.jay@gmail.com>
2025-02-02 17:30:18 -0600

    Added display attriubtes to match the case study

--- SportsPro/Models/Product.cs
+++ SportsPro/Models/Product.cs
@@ -9,17 +9,26 @@ namespace SportsPro.Models
         public int ProductID { get; set; }
 
         [Required]
+        [Display(Name = "Code")]
         public string ProductCode { get; set; } = string.Empty;
 
         [Required]
+        [Display(Name = "Name")]
+
         public string Name { get; set; } = string.Empty;
 
         [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "Yearly price must be greater than zero.")]
         [Column(TypeName = "decimal(8,2)")]
+        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = false)]
+        [Display(Name = "Price")]
+
         public decimal YearlyPrice { get; set; }
 
         [Required]
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+
         public DateTime ReleaseDate { get; set; } = DateTime.Now;
     }
 }
