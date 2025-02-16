d8598cd02d0c07e901c099b938d1d6990061fd6e
Sif-Ragnos <beth.eckert@southeasttech.edu>
2025-02-16 14:24:22 -0600

    Added Routing???

--- SportsPro/Controllers/HomeController.cs
+++ SportsPro/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ namespace SportsPro.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("/")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("About")]
         public IActionResult About()
         {
             return View();
